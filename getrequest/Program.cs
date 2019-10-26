2ac4fd784d17be1b711e557fcde0ee6628967e3e
Guillermo Navarro <gnavarro0197@gmail.com>
2019-10-25 23:27:04 -0500

    C# Visual Studio COde

---
+++ getrequest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace getrequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            string API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+            var cliente = new WebClient();
+            var json = cliente.DownloadString(API_URL);
+            //Console.WriteLine(json);
+            dynamic posts = JsonConvert.DeserializeObject(json);
+
+            foreach(var post in posts){
+                Console.WriteLine(post.id + ": " + post.title);
+            }
+        }
+    }
+}
